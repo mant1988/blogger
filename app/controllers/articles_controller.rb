f7f2ff7e66e78ab16ae1e9d51a15efb11e1b7d41
tour <tour@intropica.com>
2013-08-08 17:29:58 +0700

    add new method for new erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,28 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path
+  end
+
   def show
     @article = Article.find_by_id(params[:id])
   end
+
+  def destroy
+
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
