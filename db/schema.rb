c017ff6fa7f4ee07cb41055d5b7f58e404785414
tour <tour@intropica.com>
2013-08-09 12:00:51 +0700

    begin with comment, but not finish now

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130808095011) do
+ActiveRecord::Schema.define(version: 20130809031425) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.integer  "article_id"
+    t.string   "author_name"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
