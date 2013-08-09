fad3b6f28522d492ff2d096003919e5565c1339b
tour <tour@intropica.com>
2013-08-09 09:42:40 +0700

    make edit article and move partial to new and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  # before_filter :find_article , :except => [:new, :create]
+
   def index
     @articles = Article.all
   end
@@ -15,17 +17,35 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def edit
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def update
+    @article = Article.find_by_id(params[:id])
+    @article.update_attributes(article_params)
+
+    redirect_to articles_path
+  end
+
   def show
     @article = Article.find_by_id(params[:id])
   end
 
   def destroy
-    @article = Article.find_by_id(params[:id]).delete
+    @article.delete
 
     redirect_to articles_path
   end
 
   private
+
+  # def find_article
+  #   @article = Article.find_by_id(params[:id])
+  #   # unless @article
+  #   #   redirect_to articles_path
+  #   # end
+  # end
   def article_params
     params.require(:article).permit(:title, :body)
   end
