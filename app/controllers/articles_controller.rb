1910b0b2bbce5ed9da79562c29c73c472bcbe673
tour <tour@intropica.com>
2013-08-09 10:07:41 +0700

    refactoring code by make find_article method for @article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  # before_filter :find_article , :except => [:new, :create]
+  before_filter :find_article , :only => [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -14,38 +14,42 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created! "
+
     redirect_to articles_path
   end
 
   def edit
-    @article = Article.find_by_id(params[:id])
   end
 
   def update
-    @article = Article.find_by_id(params[:id])
     @article.update_attributes(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated! "
 
     redirect_to articles_path
   end
 
   def show
-    @article = Article.find_by_id(params[:id])
   end
 
   def destroy
     @article.delete
+
+    flash.notice = "Article was Deleted! "
 
     redirect_to articles_path
   end
 
   private
 
-  # def find_article
-  #   @article = Article.find_by_id(params[:id])
-  #   # unless @article
-  #   #   redirect_to articles_path
-  #   # end
-  # end
+  def find_article
+    @article = Article.find_by_id(params[:id])
+    unless @article
+      redirect_to articles_path and return
+    end
+  end
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
